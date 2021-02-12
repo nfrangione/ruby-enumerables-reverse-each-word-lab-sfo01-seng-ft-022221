20a83f195a9b031f6c8bb781d3213ef18de0efc9
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-12 22:45:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  words =[]
+  words = []
   words << sentence.split(" ")
   words.collect do |word|
-    word = word.reverse
+    word = word.reverse!
   end
   words.join(" ")
 end
